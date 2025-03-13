2d48e349dd1c23fcd950ccdc3fc5b03c6359ee2c
ziad49 <136318606+ziad49@users.noreply.github.com>
2025-03-13 11:56:58 +0100

    Create app.js

---
+++ Admin/app.js
@@ -0,0 +1,10 @@
+import 'bootstrap/dist/css/bootstrap.min.css';
+import 'bootstrap/dist/js/bootstrap.bundle.min';
+
+import './bootstrap';
+
+import Alpine from 'alpinejs';
+
+window.Alpine = Alpine;
+
+Alpine.start();
