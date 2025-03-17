1e208d21eac48242b1c182d0ee772ea7a2d2b5c6
Ekang1203 <olivieraho12@gmail.com>
2025-03-17 11:31:42 +0100

    Update Projet.php

--- Admin/Projet.php
+++ Admin/Projet.php
@@ -1,13 +1,1 @@
-<?php
 
-namespace App\Models;
-
-use Illuminate\Database\Eloquent\Factories\HasFactory;
-use Illuminate\Database\Eloquent\Model;
-
-class Projet extends Model
-{
-    use HasFactory;
-
-    protected $fillable = ['titre', 'description'];
-}
